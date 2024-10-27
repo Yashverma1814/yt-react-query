d073f311210c5ee1aa61fce7ff41133c14c37293
Yash Verma <yashv1752@gmail.com>
2024-10-27 15:55:12 +0530

    woeking on tanstack query and doing practice of it

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,85 +1,79 @@
-import { useQuery } from '@tanstack/react-query';
-// import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-const fetchProducts = async () => {
-    const response = await fetch('https://dummyjson.com/products');
-    const data = await response.json();
-    return data.products;
-};
+const fetchProducts = async() =>{
+    const res = await fetch('https://dummyjson.com/products');
+    const data = await res.json()
+    return data.products
+}
 
 const Products = () => {
-    const {
-        isLoading,
-        error,
-        data: products,
-    } = useQuery({
-        queryKey: ['products'],
-        queryFn: fetchProducts,
-        // staleTime: 10000,
-    });
 
-    // const [products, setProducts] = useState([]);
-    // const [isLoading, setIsLoading] = useState(false);
+    const {isLoading,error,data:products} = useQuery({queryKey:["products"],queryFn:fetchProducts})
+    
+    // const [products, setProducts] = useState([])
+    // const [loading,setLoading] = useState(false)
     // const [error, setError] = useState(null);
 
-    // useEffect(() => {
-    //     const fetchProducts = async () => {
+    // useEffect(()=>{
+    //     const fetchProducts = async() =>{
     //         try {
-    //             setIsLoading(true);
-    //             setError(null);
-    //             const response = await fetch('https://dummyjson.com/product');
-    //             const data = await response.json();
-    //             console.log(data.products);
-    //             setProducts(data.products);
-    //             setIsLoading(false);
+    //             setLoading(true)
+    //             setError(null)
+    //             const res = await fetch('https://dummyjson.com/productss');
+    //             const data = await res.json()
+    //             setProducts(data.products)
+    //             setLoading(false)    
     //         } catch (err) {
-    //             setError(err.message);
-    //             setIsLoading(false);
+    //             setLoading(false)
+    //             setError(err.message)
     //         }
-    //     };
+            
+    //     }
     //     fetchProducts();
-    // }, []);
+    // },[])
 
-    if (isLoading) {
-        return <h3>Loading...</h3>;
+    if(isLoading){
+        return <h1>Loading...</h1>
     }
 
-    if (error) {
-        return <h3>Error: {error.message}</h3>;
+    if(error){
+        return <h2>Error: {error}</h2>
     }
 
     return (
-        <div className="bg-white">
-            <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-                    Customers also purchased
-                </h2>
+        <div>
+            <div className="bg-white">
+                <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                    <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
 
-                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                    <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                     {products.map((product) => (
                         <div key={product.id} className="group relative">
-                            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                                <img
-                                    src={product.thumbnail}
-                                    alt={product.title}
-                                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
-                                />
+                        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                            <img
+                            alt={product.title}
+                            src={product.thumbnail}
+                            className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                            />
+                        </div>
+                        <div className="mt-4 flex justify-between">
+                            <div>
+                            <h3 className="text-sm text-gray-700">
+                                <Link to={`/products/${product.id}`}>
+                                <span aria-hidden="true" className="absolute inset-0" />
+                                {product.title}
+                                </Link>
+                            </h3>
+                            <p className="mt-1 text-sm text-gray-500">{product.category}</p>
                             </div>
-                            <div className="mt-4 flex justify-between">
-                                <div>
-                                    <h3 className="text-sm text-gray-700">
-                                        <Link to={`/products/${product.id}`}>
-                                            <span aria-hidden="true" className="absolute inset-0" />
-                                            {product.title}
-                                        </Link>
-                                    </h3>
-                                    <p className="mt-1 text-sm text-gray-500">{product.category}</p>
-                                </div>
-                                <p className="text-sm font-medium text-gray-900">{product.price}</p>
-                            </div>
+                            <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                        </div>
                         </div>
                     ))}
+                    </div>
                 </div>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -24,16 +24,15 @@ const router = createBrowserRouter([
 ]);
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 10000,
-        },
-    },
-});
+    defaultOptions:{
+        queries:{
+            staleTime:10000,
+        }
+    }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
+        <RouterProvider router={router} />  
     </QueryClientProvider>
 );
